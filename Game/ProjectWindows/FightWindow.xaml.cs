69f0ccfaaa019e6a08ec4e1f63044e798d867856
Artem <artemermolaev0@gmail.com>
2020-09-08 15:30:15 +0300

    Add character Window fixes

--- Game/ProjectWindows/FightWindow.xaml.cs
+++ Game/ProjectWindows/FightWindow.xaml.cs
@@ -22,7 +22,7 @@ namespace Game.ProjectWindows
         public bool switcher { get; set; }
         Hero firstHero = new Hero()
         { basicDamage = 10, critChance = 25, critPower = 110, hp = 150, name = "Gayman", protection = 35, vortex = 10 };//Test heroes
-                                                                                                                        // ЭТО НАДО УДАЛИТЬ!!!
+                                                                                                                        // ЭТО НАДО УДАЛИТЬ!!! yt afrn
         Hero secondHero = new Hero()
         { basicDamage = 15, critChance = 50, critPower = 230, hp = 80, name = "Vovaman", protection = 10, vortex = 25 };//Test heroes
 
@@ -40,13 +40,13 @@ namespace Game.ProjectWindows
 
         private void NextTurnBtn_Click(object sender, RoutedEventArgs e)
         {
-
             BattleClass battleClass = new BattleClass();
             BattleLogTb.Text += battleClass.BattleFunction(firstHero,secondHero);
             Hero1HpLb.Content = Math.Round(firstHero.hp, MidpointRounding.ToEven);
             Hero2HpLb.Content = Math.Round(secondHero.hp, MidpointRounding.ToEven);
             Hero1Pb.Value = firstHero.hp;
             Hero2Pb.Value = secondHero.hp;
+            ScrollViewer.ScrollToBottom();
         }
     }
 }
